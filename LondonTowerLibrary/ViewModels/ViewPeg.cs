6dc23f4975028066928301542aaa714c4d38479f
sbully <43850116+sbully@users.noreply.github.com>
2019-12-17 16:13:50 +0100

    Commentaire ViewPeg et ViewTrial

--- LondonTowerLibrary/ViewModels/ViewPeg.cs
+++ LondonTowerLibrary/ViewModels/ViewPeg.cs
@@ -6,22 +6,42 @@ using System.Windows.Media.Imaging;
 
 namespace LondonTowerLibrary.ViewModels
 {
+    /// <summary>
+    /// Classe ViewPeg representation graphique d'un Peg dans sa position et hauteur
+    /// Heritage de la classe Image la classe ViewPeg étant une simple représentation graphique
+    /// </summary>
     public class ViewPeg : Image
     {
-        private Peg peg;
+        /// <summary>
+        /// Liste des ViewBead, representation graphique des Beads empilé sur le Peg
+        /// </summary>
         private List<ViewBead> ListViewBead;
-
+        /// <summary>
+        /// Classe metié representant un pique/tour sur le jeu London Tower où l'on doit empilé des Beads/perles
+        /// </summary>
+        private Peg peg;
         public Peg Peg
         {
             get { return peg; }
             set { peg = value; }
         }
 
+        /// <summary>
+        /// Constructeur par défaut en privé pour forcer la construction avec le constructeur paramétré.
+        /// Initialisation de la list de ViewBead.
+        /// </summary>
         private ViewPeg()
         {
             ListViewBead = new List<ViewBead>();
         }
 
+        /// <summary>
+        /// Appel du constructeur par defaut pour l'initialisation de de la liste de ViewBead.
+        /// Initialisation de l'image source de la classe.
+        /// Positionnement du Peg dans son environnement.
+        /// </summary>
+        /// <param name="_peg"></param>
+        /// <param name="_visualHelp"></param>
         public ViewPeg(Peg _peg, bool _visualHelp) : this()
         {
             peg = _peg;
@@ -38,27 +58,39 @@ namespace LondonTowerLibrary.ViewModels
             CreateListViewBead(_visualHelp);
 
         }
-
+        /// <summary>
+        /// Fonction pour selectionné le ViewBead le plus haut graphiquement (derniere place dans la liste)
+        /// </summary>
+        /// <returns>si la list ListViewBead est vide retourne null, sinon retourne le dernier ViewBead de cette liste</returns>
         public ViewBead GetViewBeadUpper()
         {
             if (this.ListViewBead.Count > 0)
-            {
-                //return ListViewBead[ListViewBead.Count-1];
+            {               
                 return ListViewBead.LastOrDefault();
             }
-            return null; 
+            return null;
         }
-
+        /// <summary>
+        /// Recupere le numero du Peg stocké dans la classe 
+        /// </summary>
+        /// <returns>retourne le numero du peg</returns>
         public int PegNumber()
         {
             return peg.PegNumber;
         }
-
+        /// <summary>
+        /// Ajout d'un ViewBead à la liste ListViewBead
+        /// </summary>
+        /// <param name="_vbead">ViewBead à ajouter</param>
         public void AddViewBead(ViewBead _vbead)
         {
-                this.ListViewBead.Add(_vbead);
+            this.ListViewBead.Add(_vbead);
         }
 
+        /// <summary>
+        /// Supprime le dernier ViewBead dans la liste ListViewBead
+        /// </summary>
+        /// <returns>retourne le ViewBead supprimé si la Liste n'est pas vide, sinon retourne null</returns>
         public ViewBead RemoveTopViewBead()
         {
             ViewBead lastBead = ListViewBead.LastOrDefault();
@@ -69,15 +101,23 @@ namespace LondonTowerLibrary.ViewModels
             }
             return null;
         }
-
+        /// <summary>
+        /// Retourne la liste de ViewBead
+        /// </summary>
+        /// <returns> retourne ListViewBead </returns>
         public List<ViewBead> GetListViewBead()
         {
             return this.ListViewBead;
         }
 
+        /// <summary>
+        /// Création des ViewBead, représentation graphique des Beads empilés sur le Peg
+        /// </summary>
+        /// <param name="_visualhelp">Booléen représentant l'option d'affichage des aides visuels, false pour aucune aide
+        /// true pour l affichage de la premiere lettre de la couleur sur le Bead</param>
         private void CreateListViewBead(bool _visualhelp)
         {
-            for(int i=0; i<peg.BeadList.Count; i++)
+            for (int i = 0; i < peg.BeadList.Count; i++)
             {
                 ViewBead vb = new ViewBead(peg.BeadList[i], _visualhelp);
                 vb.Row = i;
@@ -85,10 +125,21 @@ namespace LondonTowerLibrary.ViewModels
             }
         }
 
+        /// <summary>
+        /// Verification de la possibilité d'ajouter un Bead sur le Peg
+        /// </summary>
+        /// <returns>Retourne le résultat de l'appel la fonction <c>CanAdd() qui retourne un boolean s'il est 
+        /// possible d'ajouter un Bead a la liste</c></returns>
         public bool CanAddViewBead()
         {
             return this.peg.CanAdd();
         }
+
+        /// <summary>
+        /// Verification de la possibilité d'enlever un Bead sur le Peg
+        /// </summary>
+        /// <returns>Retourne le résultat de l'appel de la fonction <c>CanRemove()</c> du peg qui retourne 
+        /// un boolean s'il est possible de supprimé un Bead de la liste</returns>
         public bool CanRemoveViewBead()
         {
             return this.peg.CanRemove();
