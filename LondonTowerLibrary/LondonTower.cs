5b87150295be1a797aa97dad9e3b50fe42dedc75
sbully <43850116+sbully@users.noreply.github.com>
2019-12-20 10:17:02 +0100

    Maj Commentaire

--- LondonTowerLibrary/LondonTower.cs
+++ LondonTowerLibrary/LondonTower.cs
@@ -16,12 +16,34 @@ namespace LondonTowerLibrary
         private DateTime dateAndTime;
         private string fdback;
 
+        /// <summary>
+        /// Entier représentant le niveau de difficulté correspondant aux nombres de Pegs (3,4 ou 5)
+        /// </summary>
         public int Level { get => level; set => level = value; }
 
+        /// <summary>
+        /// Liste contenant tous les Trials d'un niveau de difficulté
+        /// </summary>
         public List<Trial> TrialList { get => trialList; set => trialList = value; }
+
+        /// <summary>
+        /// Object contenant les informations de l'utilisateurs actuel
+        /// </summary>
         public Personn Personn { get => personn; set => personn = value; }
+
+        /// <summary>
+        /// Booleen représentant l'option d'aide visuel modifiant le rendu visuel des Beads 
+        /// </summary>
         public bool VisualHelp { get => visualHelp; set => visualHelp = value; }
+
+        /// <summary>
+        /// Date et Heure du lancement du test London Tower
+        /// </summary>
         public DateTime DateAndTime { get => dateAndTime; set => dateAndTime = value; }
+
+        /// <summary>
+        /// String contenant le ressenti de l'utilisateur à la fin du test LondonTower
+        /// </summary>
         public string Fdback { get => fdback; set => fdback = value; }
 
         /// <summary>
